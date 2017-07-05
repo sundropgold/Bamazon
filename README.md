2934e549fa3c954d11db803820c871aa1d3165de
Flynn <flynnigan.tan@gmail.com>
2017-07-05 16:38:35 -0500

    fixed numbers of products and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Bamazon
+
+> 1) After creation of the Bamazon database and products table, this is what the products table looks like.
+![initial products table](images/screenshot_start.png)
+
+> 2) When the user commands to run "node BamazonCustomer.js", this is the view of the products they will be seeing.
+![node products view part 1](images/screenshot_nodeproducts1.png)
+
+![node products view part2](images/screenshot_nodeproducts2.png)
+
+> 3) When the user enters a quantity to purchase that is over the available stock quantity, they get this message, and the products table is not altered at all.
+![user quantity over stock quantity](images/screenshot_unsuccessful.png)
+
+> 4) The table still looks the same after this.
+![user quantity over stock quantity 2](images/screenshot_unsuccessfultable.png)
+
+> 5) When the user enters a product whose quantity is also available in the stock quantity, the transaction will go through. The stock quantity is updated in the products table, and the user will receive a receipt of the item they purchased.
+![user purchase success](images/screenshot_successful)
+
+> 6) The table will be updated at the ID of the user's chosen product. The stock number will go down by how much quantity the user purchased.
+![user purchase success](images/screenshot_successfultable)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,7 +20,10 @@ connection.connect(function(err){
 
 	askCustomer();
 
-var displayProducts = function() {
+});
+
+function askCustomer() {
+
 	// display all the items available for sale
 
 	// ids, names, department, prices
@@ -32,95 +35,76 @@ var displayProducts = function() {
 		console.log("\n WELCOME TO BAMAZON ====================================");
 
 		for (var i = 0; i <res.length; i++) {
-			console.log("\nProduct ID: " + res[i].item_id + "  |  Product Name: " 
-				+ res[i].product_name + "  |  Department Name: " 
-				+ res[i].department_name + "  |  Price: $" + res[i].price);
+			console.log("\nProduct ID: " + res[i].item_id + " | Product Name: " 
+				+ res[i].product_name + "\nDepartment Name: " 
+				+ res[i].department_name + " | Price: $" + res[i].price + "\n\n");
 		}
-
-		return res;
-	});
-};
-
-var askCustomer = function() {
-
-	// display products for sale
-	displayProducts();
 
 	inquirer.prompt([
 
-			// ask user for the id of the product they would like to purchase
-			{
-				type:"input",
-				name:"id",
-				message:"Please enter the id of the product you would like to purchase."
-			},
+		// ask user for the id of the product they would like to purchase
+		{
+			type:"input",
+			name:"id",
+			message:"Please enter the id of the product you would like to purchase."
+		},
 
-			// ask how many units of the product they would like to buy?
-			{
-				type:"input",
-				name:"quantity",
-				message:"How many units of the product would you like to purchase?"
-			}
-			]).then(function(user){
+		// ask how many units of the product they would like to buy?
+		{
+			type:"input",
+			name:"quantity",
+			message:"How many units of the product would you like to purchase?"
+		}
+		]).then(function(user){
 
-					// check if the store has enough of the product
-					// cycle through res to find the right id
-					for (var i = 0; i < res.length; i++) {
+			var userID = parseInt(user.id);
+			var userQuantity = parseInt(user.quantity);
 
-						if (user.id === res[i].item_id) {
+			// check if the store has enough of the product
+			// cycle through res to find the right id
+			for (var i = 0; i < res.length; i++) {
 
-							if (user.quantity > res[i].stock_quantity) {
-								console.log ("Sorry, insufficient quantity available. Directing you back to products...");
+				if (userID === (res[i].item_id - 1)) {
 
-								askCustomer();
-							}
+					if (userQuantity > res[i].stock_quantity) {
 
-							else if (user.quantity <= res[i].stock_quantity) {
+						// order won't go through - insufficient quantity
+						console.log ("Sorry, insufficient quantity available.");
 
-								// get new stock quantity
-								var newStock = res[i].stock_quantity - user.quantity;
-
-								// if there's enough in stock, update the database
-								connection.query("UPDATE products SET ? WHERE ?", [{
-									stock_quantity: newStock
-									},{
-									item_id: user.id
-								}], function(err, res){
-									if(err) throw err;
-
-									// tally total = quantity * price
-									var total = user.quantity * res[i].price;
-
-									// print receipt
-									console.log("Thank you for your purchase!");
-									console.log("============================");
-									console.log("Total: $" + total);
-							});
-
-						}
 					}
-				
+
+					else if (userQuantity <= res[i].stock_quantity) {
+
+						// get new stock quantity
+						var newStock = res[i].stock_quantity - userQuantity;
+
+						// tally total = quantity * price
+						var total = userQuantity * res[i-1].price;
+
+						var productName = res[i-1].product_name;
+
+						// if there's enough in stock, update the database
+						connection.query("UPDATE products SET ? WHERE ?", [{
+							stock_quantity: newStock
+							},{
+							item_id: userID
+						}], function(err, res){
+							if(err) throw err;
+
+							// print receipt
+							console.log("\n\nThank you for your purchase!");
+							console.log("============================");
+							console.log("Total of " + productName + ": $" + total);
+
+						});
+
+					}
 				}
+			} // for loop end
 
-				inquirer.prompt([
-						{
-							type:"confirm",
-							name:"continue",
-							message:"Would you like to continue shopping?"
-							default:true
-						}
+		}); // product prompt end
 
-					]).then(function(user){
-
-						if (user.continue === true){
-							console.log("Directing you back to products... ");
-							askCustomer();
-						}
-						else {
-							console.log("Have a magical day!!");
-						}
-					});
-
-			});
-
+	}); // connection query end
 };
+
+
